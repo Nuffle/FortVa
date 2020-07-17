3d9be7dfaf4ae828b86a45109042b57ce9a70efb
Glitch (hello-express) <none>
2020-07-17 19:49:41 +0000

    🎿🚏 Checkpoint ./index.js:19278672/30

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.on("guildMemberAdd", async (member) => {
 
 client.on("ready", () => {
   let activities = [
-      `Utilize ${config.prefix}ajuda para obter ajuda`,
+      `Utilize ${config`,
       `${client.guilds.cache.size} servidores!`,
       `${client.channels.cache.size} canais!`,
       `${client.users.cache.size} usuários!`
